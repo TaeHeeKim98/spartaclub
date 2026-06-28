0d696db3ebf6d7a6552d917f8153dc47229a3445
TaeHeeKim <eksrnsdhkdwk@gmail.com>
2026-06-29 00:37:52 +0900

    oprhanRemoval 적용

--- spring숙련/jpa-advance/src/main/java/com/sparta/jpaadvance/entity/User.java
+++ spring숙련/jpa-advance/src/main/java/com/sparta/jpaadvance/entity/User.java
@@ -17,7 +17,7 @@ public class User {
     private Long id;
     private String name;
 
-    @OneToMany(mappedBy = "user",cascade = {CascadeType.PERSIST, CascadeType.REMOVE}/*, cascade = CascadeType.PERSIST*/)
+    @OneToMany(mappedBy = "user", cascade = CascadeType.PERSIST, orphanRemoval = true /*,cascade = {CascadeType.PERSIST, CascadeType.REMOVE}*/)
     private List<Food> foodList = new ArrayList<>();
 
     public void addFoodList(Food food) {
